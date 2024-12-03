3109b71a7da20bf3cd676514729fd913a98daf0f
root <root@DESKTOP-GS7IFRL>
2024-12-03 22:04:53 +0800

    add server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import request, jsonify, Flask
+import init
+
+
+sd = lambda: init.get_stardict()
+
+app = Flask(__name__)
+
+
+@app.route('/get')
+def index():
+    word = request.args.get("word")
+    result = sd().query(word)
+    return jsonify(result)
+
+
+app.run(host="0.0.0.0", port=8080)
